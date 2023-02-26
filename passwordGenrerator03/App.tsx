76b373f94febd4b89ca1ffbfce45d5a2f1045539
Moeen <imoeen7@gmail.com>
2023-02-26 13:52:34 +0530

    added the boilerplate validating the inputs and generate the passwords

--- passwordGenrerator03/App.tsx
+++ passwordGenrerator03/App.tsx
@@ -1,28 +1,44 @@
-import React from 'react';
-import {
-  Text,
-  View,
-} from 'react-native';
+import React, {useState} from 'react';
+import {Text, View} from 'react-native';
 
 // used for validation of numbers, strings, etc
-import * as Yup from 'yup'
+import * as Yup from 'yup';
 
-// yup schema for validdating the passwords for our app 
-const passwordSchema = Yup.object().shape(
-  {
-    passwordLength: Yup.number()
+// yup schema for validating the passwords
+const passwordSchema = Yup.object().shape({
+  passwordLength: Yup.number()
     .min(6, 'should be atleast 6 chars long')
     .max(12, 'should be max 12 chars long')
-    .required("Length is required")
-  }
-)
+    .required('Length is required'),
+});
 
-function App(){
+function App() {
+  // password: the variable that stores the actual value
+  // setPassword: function that updates the value of the 'passWord'
+  const [password, setPassword] = useState('');
+  const [isPasswordGenerated, setIsPasswordGenerated] = useState(false)
+  const [lowerCase, setLowerCase] = useState(true)
+  const [upperrCase, setupperrCase] = useState(false)
+  const [numbers, useNumbers] = useState(false)
+  const [symbols, useSymbols] = useState(false)
+
+  const generatePasswordString = (passwordLength: number) =>{
+    // magic will happend here
+  }
+
+  const createPasword = (characters: string, passwordLength:number) => {
+    // 
+  }
+
+  const resetPasswordState = () => {
+    // 
+  }
+  
   return (
     <View>
       <Text>password generator</Text>
     </View>
-  )
-};
+  );
+}
 
 export default App;
