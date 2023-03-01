74770e0aea3bda2cb93b64d2addfa80e8d4b0e1c
Moeen <imoeen7@gmail.com>
2023-03-02 00:30:51 +0530

    added logic for generating the random characters that will be used in the password

--- passwordGenrerator03/App.tsx
+++ passwordGenrerator03/App.tsx
@@ -27,7 +27,12 @@ function App() {
   };
 
   const createPasword = (characters: string, passwordLength: number) => {
-    //
+    let result = ''
+    for (let i = 0; i < passwordLength; i++) {
+      const characterIndex = Math.round(Math.random() * characters.length);
+      result += characters.charAt(characterIndex);      
+    }
+    return result;
   };
 
   const resetPasswordState = () => {
